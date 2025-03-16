556066f975226b2d9f2ed17f537a7873e8f38758
Omar Abd Albaqi <omar@Omars-MacBook-Pro.local>
2025-03-16 22:54:31 +0300

    qwerty2

--- lib/providers/home_providers/filters/filter_provider.dart
+++ lib/providers/home_providers/filters/filter_provider.dart
@@ -90,9 +90,6 @@ class FilterProvider with ChangeNotifier {
     // notifyListeners();
   }
 
-
-  void test (){}
-
   // Function to save the current state of the filters (for reset)
   void saveCurrentState() {
     selectedParams = Map.from(filterParams); // Save the current state to restore later
